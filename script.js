006e8f633ed78cdf64cdd58fad9c933bdb011448
shery <sheryarking5@gmail.com>
2022-07-13 19:58:38 +0500

    skill section complete

--- script.js
+++ script.js
@@ -23,3 +23,27 @@ $(document).ready(function(){
         $('.menu').toggleClass("active");
         $('.menu-btn i').toggleClass("active");
     });
+
+// sills circular bar
+    const card = document.querySelectorAll('.card');
+window.addEventListener('load', function(){
+  card.forEach(item => {
+    let numElement = item.querySelector('.number');
+    let num = parseInt(numElement.innerText);
+    let count = 0;
+    let time = 2000 / num;
+    let circle = item.querySelector('.circle');
+    setInterval(() => {
+      if(count == num){
+        clearInterval();
+      } else {
+        count += 1;
+        numElement.innerText = count + "%";
+      }
+    }, time)
+    circle.style.strokeDashoffset 
+      = 235 - ( 235 * ( num / 100 ));
+   
+    
+  })
+});
